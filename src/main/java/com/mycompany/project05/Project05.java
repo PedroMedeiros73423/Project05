f934b3d0229839e6ca508ae70f428d07a9fa70d8
PedroMedeiros7631 <81970006+PedroMedeiros73423@users.noreply.github.com>
2024-09-21 14:37:19 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/project05/Project05.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.project05;
+
+public class Project05 {
+
+    public static void main(String[] args) {
+        int numero;
+        numero = 54;
+        System.out.println("O valor da variável é: " + numero);
+    }
+}
